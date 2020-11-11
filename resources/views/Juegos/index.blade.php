872a48f9e74aaaffaff1113ecc63beedc0862f54
jorgeesc <jorge.tetrix@gmail.com>
2020-11-11 13:39:36 -0600

    tamaño de formulario

--- resources/views/Juegos/index.blade.php
+++ resources/views/Juegos/index.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-15">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">{{ __('Juegos') }}</div>
 <div class="card-body">
